1d9218169355e3d9be0672dcd9adf14ced545214
David Cespedes <ingdavidcespedes@gmail.com>
2016-10-27 10:42:39 -0500

    First commit

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by David Andres Cespedes on 10/26/16.
+//  Copyright © 2016 David Andres Cespedes. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
